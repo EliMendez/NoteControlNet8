1ca6be69f18208a01dc83ed2b19e91b232780e3f
Elizabeth Méndez <elixa_mendex@hotmail.com>
2024-07-30 18:29:49 -0600

    Agregar archivos de proyecto.

---
+++ NoteControlNet8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NoteControlNet8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
