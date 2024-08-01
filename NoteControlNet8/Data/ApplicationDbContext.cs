fb844884b645617aaf1b2305f46d5f8496e33e67
Elizabeth Méndez <elixa_mendex@hotmail.com>
2024-08-01 15:11:27 -0600

    Add database connection

---
+++ NoteControlNet8/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NoteControlNet8.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+    }
+}
--- NoteControlNet8/Program.cs
+++ NoteControlNet8/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using NoteControlNet8.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Add SqlServer connection
+var connectionStrig = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionStrig));
 
 var app = builder.Build();
 
